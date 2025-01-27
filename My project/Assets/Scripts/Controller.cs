cce2919d9dde603d201af2fc161f2506d5c62b10
Teroflups <alexanderyugay2007@gmail.com>
2025-01-27 15:57:54 +0300

    prefs + reload scene

--- My project/Assets/Scripts/Controller.cs
+++ My project/Assets/Scripts/Controller.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
     [SerializeField] private int _coins;
     [SerializeField] private int _rate;
     [SerializeField] private TextMeshProUGUI _currentText;
-    
+
     [SerializeField] private Button _buttonClick;
     [SerializeField] private Button _buttonUpgrade;
 
@@ -18,6 +19,8 @@ public class Controller : MonoBehaviour
         _buttonClick.onClick.AddListener(Click);
         _buttonUpgrade.onClick.AddListener(UpgradeClick);
 
+        _coins = PlayerPrefs.GetInt("Coins", 0);
+        _rate = PlayerPrefs.GetInt("Rate", 1);
     }
     private void Update()
     {
@@ -26,19 +29,34 @@ public class Controller : MonoBehaviour
     private void Click()
     {
         _coins += _rate;
+        PlayerPrefs.SetInt("Coins", _coins);
     }
 
     private void UpgradeClick()
     {
-        if(_coins >= 15)
+        if (_coins >= 15)
         {
             _coins -= 15;
             _rate += 1;
+
+            PlayerPrefs.SetInt("Coins", _coins);
+            PlayerPrefs.SetInt("Rate", _rate);
         }
     }
     private void OnDestroy()
     {
         _buttonClick.onClick.RemoveListener(Click);
         _buttonUpgrade.onClick.RemoveListener(UpgradeClick);
+    }
+
+    public void RefreshLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void PLayerPrefs()
+    {
+        PlayerPrefs.SetInt("Coins", 0);
+        PlayerPrefs.SetInt("Rate", 1);
     }
 }
