c9ab1e96b6a63f83d4d7a13a9d585f855cad5d16
Teroflups <alexanderyugay2007@gmail.com>
2025-01-29 13:43:38 +0300

    move

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rb;
+    private Vector2 _moveVelocity;
+
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _moveVelocity = moveInput.normalized * _speed; 
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveVelocity * Time.fixedDeltaTime);
+    }
+}
